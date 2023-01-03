24e4079cbaf00d884af0ee4ee45d5e223ac0bf49
Mr-Mesut-OZTURK <mesut8311006@gmail.com>
2023-01-03 15:35:35 +0300

    created project

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    unoptimized: true,
+    // loader: 'custom',
+  },
+  devIndicators: {},
+  publicRuntimeConfig: {
+    // Available on both server and client
+    theme: "DEFAULT",
+    currency: "USD",
+  },
+};
